c2f3d98d8e0d7dd421fcbf7af129def06da0c774
JUSTFIDEL <fidenki1429@gmail.com>
2023-02-17 21:42:23 +0100

    Initial Commit

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import Counter from './Counter';
+
+function App() {
+	return (
+		<div>
+			<Header name='JustFidel' />
+			<Counter />
+		</div>
+	);
+}
+
+export default App;
